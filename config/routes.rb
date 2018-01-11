296f5ef516f7b25d6d0bf1cc89ac6e040f435a49
Muamer Ribica <muamer.ribica@gmail.com>
2018-01-11 21:01:37 +0100

    Add user create and show actions with token authorization

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   devise_scope :user do
     root to: 'doorkeeper/applications#index'
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[create show]
+    end
+  end
 end
